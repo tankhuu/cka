04952a281ff9f1546d066d3963006f9cb51cc4b0
Tan Khuu <jackiekhuu.work@gmail.com>
2023-11-14 22:48:59 +0700

    add sg

--- setup-vanilla-k8s/control-plane.tf
+++ setup-vanilla-k8s/control-plane.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "cp1" {
+resource "aws_instance" "control_plane" {
   count         = var.control_plane_count
   ami           = var.ami_id
   instance_type = var.cp_instance_type
@@ -15,6 +15,25 @@ resource "aws_instance" "cp1" {
     volume_type = "standard"
   }
 
-  security_groups = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.cp_sg.id]
+}
+
+resource "aws_security_group" "cp_sg" {
+  name        = "cp_sg"
+  description = "Control Plane inbound traffic"
+  # vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "All TCP from Anywhere"
+    from_port        = 0
+    to_port          = 65535
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "cp_sg"
+  }
 }
 
--- setup-vanilla-k8s/workers.tf
+++ setup-vanilla-k8s/workers.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "worker1" {
+resource "aws_instance" "worker" {
   count         = var.worker_count
   ami           = var.ami_id
   instance_type = var.worker_instance_type
@@ -14,6 +14,25 @@ resource "aws_instance" "worker1" {
     volume_type = "standard"
   }
 
-  security_groups = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.worker_sg.id]
 
+}
+
+resource "aws_security_group" "worker_sg" {
+  name        = "worker_sg"
+  description = "Control Plane inbound traffic"
+  # vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "All TCP from Anywhere"
+    from_port        = 0
+    to_port          = 65535
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "worker_sg"
+  }
 }
