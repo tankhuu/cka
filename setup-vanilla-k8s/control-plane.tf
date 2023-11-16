d7d3480004534cdf2fdd2679a3d89e7306bd5656
Tan Khuu <jackiekhuu.work@gmail.com>
2023-11-16 21:28:44 +0700

    add egress

--- setup-vanilla-k8s/control-plane.tf
+++ setup-vanilla-k8s/control-plane.tf
@@ -32,6 +32,13 @@ resource "aws_security_group" "cp_sg" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "cp_sg"
   }
